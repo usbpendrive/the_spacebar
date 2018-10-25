8b6ff823d0684a4c25ad8e506f76e2124a06ba7a
Fernando <ferry.fernando@gmail.com>
2018-10-25 08:45:29 +0700

    Basic use of twig template engine.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,9 +10,10 @@ namespace App\Controller;
 
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @return Response
@@ -30,6 +31,8 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf('Future page to show the article: "%s"', $slug));
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug))
+        ]);
     }
 }
