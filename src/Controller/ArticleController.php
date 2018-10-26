7174196f7c5f6a4c087d0fe65af381543a80b01d
Fernando <ferry.fernando@gmail.com>
2018-10-26 09:57:11 +0700

    Using PHP APCuAdapter cache.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -60,12 +60,12 @@ strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lo
 cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
 fugiat.
 EOF;
-        dump($markdown);die;
         $item = $cache->getItem('markdown_'.md5($articleContent));
         if (!$item->isHit()) {
             $item->set($markdown->transform($articleContent));
             $cache->save($item);
         }
+        #dump($cache);die;
         $articleContent = $item->get();
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
